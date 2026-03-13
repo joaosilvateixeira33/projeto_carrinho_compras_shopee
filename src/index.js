41f5038e12e2148a9e9989c696b92e77de53c2df
João Silva <joaoteixeirasilva33@gmail.com>
2026-03-13 19:43:10 -0300

    feat: implement action to deletItem

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const myWishList = []
 
 console.log("Welcome to the your shopee cart!");
 
-const item1 = await createItem("Hotwheels Ferrari", 20.99, 1)
+const item1 = await createItem("Hotwheels Ferrari", 20.99, 10)
 const item2 = await createItem("Hotwheels Lamborghini", 39.99, 3)
 
 await cartService.addItem(myCart, item1)
-await cartService.addItem(myWishList, item2)
+await cartService.addItem(myCart, item2)
+
+// await cartService.deleteItem(myCart, item2.name)
+// await cartService.deleteItem(myCart, item1.name)
 
 console.log("Shopee cart total: ");
-
 await cartService.calculateTotal(myCart)
